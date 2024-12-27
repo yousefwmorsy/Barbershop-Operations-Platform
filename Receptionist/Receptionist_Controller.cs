d2d835b59e0175af600ebefefa1dfc7b12ad5757
Yousef Morsy <yousefwmorsy@gmail.com>
2024-12-27 19:03:02 +0200

    daysoff validation

--- Receptionist/Receptionist_Controller.cs
+++ Receptionist/Receptionist_Controller.cs
@@ -57,15 +57,22 @@ namespace Barbershop_Operations_Platform
             return (int)dbMan.ExecuteScalar(query);
         }
 
-        public int InsertDayOffRequest(int empid, string sdate, string edate)
+        public int InsertDayOffRequest(int empid, string startdate, string enddate)
         {
             int managerid = GetManagerID(empid);
-            string status = "Pending";
-            //string query = 
-            //return dbMan.ExecuteNonQuery(query);
-            return 0;
+            string query = $"INSERT INTO DaysOffRequest (EmployeeID, ManagerID, StartDate, EndDate) VALUES ({empid}, {managerid}, '{startdate}', '{enddate}')";
+            return dbMan.ExecuteNonQuery(query);
         }
 
+        public int CheckRepeatedRequests(int empid, string startdate, string enddate) {
+            string query = $"SELECT Count(*) FROM DaysOffRequest WHERE StartDate = '{startdate}' AND EndDate = '{enddate}' AND EmployeeID = {empid}";
+            return (int)dbMan.ExecuteScalar(query);
+        }
+        public DataTable ViewPrevRequests(int id)
+        {
+            string query = $"SELECT CONCAT(m.First_Name, ' ', m.Last_Name) as 'Manager Name', StartDate, EndDate, Status \r\nFROM Employee as m, DaysOffRequest\r\nWHERE m.Emp_id = ManagerID AND EmployeeID = {id}";
+            return dbMan.ExecuteReader(query);
+        }
         public void TerminateConnection()
         {
             dbMan.CloseConnection();
